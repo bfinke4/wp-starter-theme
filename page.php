5f61ca5440d6a5b0f4b4d3db4088796f807db6ea
Brian Finke <finchformmedia@gmail.com>
2017-01-05 13:06:05 -0600

    Commit

--- page.php
+++ page.php
@@ -4,8 +4,8 @@
 
 				<div id="inner-content" class="wrap cf">
 
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
+						<main id="main" class="" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+<i class="fa fa-camera-retro fa-2x">
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
