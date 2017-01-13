bf708a12105fec259649c64647a8727400f72294
Brian Finke <finchformmedia@gmail.com>
2017-01-13 16:54:01 -0600

    Styled Page Titles and Page Title Excerpts

--- page.php
+++ page.php
@@ -5,18 +5,20 @@
 				<div id="inner-content" class="">
 
 						<main id="main" class="" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-<i class="fa fa-camera-retro fa-2x">
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
 								<header class="article-header">
+									<div id="inner-article-header">
+										<div class="page-title-rule"></div>
+										<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+										<div class="page-title-excerpt">
+											<p><?php the_field('page_title_excerpt'); ?></p>
+										</div>
+									</div>
 
-									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-
-									<p class="byline vcard">
-										<?php printf( __( 'Posted', 'bonestheme').' <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> '.__( 'by',  'bonestheme').' <span class="author">%3$s</span>', get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p>
+									
 
 								</header> <?php // end article header ?>
 
